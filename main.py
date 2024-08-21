c38730d8ca2574cf00db8ce5aa801dd50d5a12c7
moths-learn <trollaar@hotmail.com>
2024-08-21 21:44:11 +0200

    i might fail with the git part

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import string
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    dict_char = {}
-    num_words, words = get_num_words(text)
-    dict_char = get_num_characters(text)
-    print(dict_char)
+    num_words = get_num_words(text)
+    dict_char = get_char_amount(text)
+    sorted_char = sorted(dict_char.items(), key=lambda item: item[1], reverse=True)
+
+    print(f"Numbers of words: {num_words}")
+    print("character amount:")
+    for char, freq in sorted_char:
+        print(f"The {char} character was found {freq} times")
+
 
 def get_num_words(text):
     words = text.split()
-    return len(words), words()
+    return len(words)
 
-def get_num_characters(words):
-    lowered_words = words.lower()
+
+def get_char_amount(text):
+    lowered_text = text.lower()
     chars = {}
     exclude = string.punctuation + string.whitespace
-    for character in lowered_words:
+    for character in lowered_text:
         if character not in exclude:
             chars[character] = chars.get(character, 0) + 1
-
+    #        chars[character] = chars.get(character, 0) + 1
     return chars
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
 
 main()
